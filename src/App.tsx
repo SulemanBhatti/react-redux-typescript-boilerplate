372afc801caa47fa2a1bc56789a916e58d4559cc
sulemanbhatti <sulemanbhatti.business@gmail.com>
2021-12-18 16:57:38 +0500

    Added react-router-dom and setup basic routing for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import { DogsBreedList } from './features/dogs/DogsBreedList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const App: React.FC = React.memo(function App() {
   return (
-    <div className="App">
-      <DogsBreedList />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/count" element={<Counter />} />
+          <Route path="/dogs" element={<DogsBreedList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 });
 
